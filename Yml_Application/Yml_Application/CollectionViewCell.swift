7f6541c242d7f4e1cc6a75d2f55e14ccb5c02124
prakruth-yml <prakruth.nagaraj@ymedialab.com>
2019-08-20 20:01:00 +0530

    Onboarding screen complete

---
+++ Yml_Application/Yml_Application/CollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  CollectionViewCell.swift
+//  Yml_Application
+//
+//  Created by Prakruth Nagaraj on 20/08/19.
+//  Copyright © 2019 Prakruth Nagaraj. All rights reserved.
+//
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var productLogo: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    let imageNames = ["mobile-70", "home-depot-mobile", "home-mob", "molekule-mobile2"]
+    let productLogoArray = ["state-farm-logo", "thd-logo", "paypal-logo", "molekule"]
+    let productTitleArray = ["State Farm", "The Home Depot", "PayPal", "Molekule"]
+    let productDescriptionArray = ["All things insurance, all things banking, all in one app.", "The ultimate power tool: A best-in-class digital experience for The Home Depot.", "Payment giant goes mobile-by-design.", "The world's first intelligent air purifier, & the app putting clean air in people's hands. "]
+    
+    
+    //    @IBOutlet weak var productBackgroundImage: UIImageView!
+//    @IBOutlet weak var productLogoImage: UIImageView!
+//    @IBOutlet weak var productTitle: UILabel!
+//    @IBOutlet weak var productDescription: UILabel!
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        contentView.backgroundColor = .green
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    
+}
--- Yml_Application/Yml_Application/ViewController.swift
+++ Yml_Application/Yml_Application/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var ymlLogo: UIImageView!
 
     override func viewDidLoad() {
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupGUI()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellToReuse", for: indexPath) as? CollectionViewCell
+        
+        
+        let imageToBackGround = UIImage(named: cell?.imageNames[indexPath.row] ?? "nil")
+        
+        cell?.image.image = UIImage(named: cell?.imageNames[indexPath.row] ?? "nil")
+        cell?.productLogo.image = UIImage(named: cell?.productLogoArray[indexPath.row] ?? "nil")
+        cell?.productDescription.text = cell?.productDescriptionArray[indexPath.row]
+        cell?.productTitle.text = cell?.productTitleArray[indexPath.row]
+        
+        return cell ?? UICollectionViewCell()
+        
+    }
+    
+    func setupGUI(){
+        getStartedButton?.layer.cornerRadius = 9.0
+        ymlLogo?.layer.cornerRadius = 20.0 //Not Working
+        
+        //let temp = UICollectionViewFlowLayout()
     }
 
 
