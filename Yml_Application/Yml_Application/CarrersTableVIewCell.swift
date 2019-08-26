2379f5abec5daa7c20f79bfd49c5f276198e23fa
prakruth-yml <prakruth.nagaraj@ymedialab.com>
2019-08-26 14:49:34 +0530

    CArrers: Video added with extensions

---
+++ Yml_Application/Yml_Application/CarrersTableVIewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CarrersTableVIewCell.swift
+//  Yml_Application
+//
+//  Created by Prakruth Nagaraj on 26/08/19.
+//  Copyright © 2019 Prakruth Nagaraj. All rights reserved.
+//
+
+import UIKit
+
+class CarrersTableVIewCell: UITableViewCell {
+    
+    @IBOutlet weak var domain: UILabel!
+    @IBOutlet weak var role: UILabel!
+    @IBOutlet weak var location: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yml_Application/Yml_Application/CarrersVC.swift
+++ Yml_Application/Yml_Application/CarrersVC.swift
@@ -15,10 +15,14 @@ class CarrersVC: UIViewController {
 
     @IBOutlet weak var videoPlayerView: UIView!
     @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     var avPlayerView: AVPlayer = AVPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        tableView.tableFooterView = UIView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,9 +32,11 @@ class CarrersVC: UIViewController {
         let avPlayerLayer = AVPlayerLayer(player: avPlayerView)
         avPlayerLayer.frame = videoPlayerView.bounds
         videoPlayerView.layer.addSublayer(avPlayerLayer)
-        let viewTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(mouseDidMoveOverView(_:)))
-        viewTapGesture.minimumPressDuration = 1.0
+        let viewTapGesture = UITapGestureRecognizer(target: self, action: #selector(mouseDidTapView(_:)))
+//        viewTapGesture.minimumPressDuration = 1.0
         videoPlayerView.addGestureRecognizer(viewTapGesture)
+//        playPauseButton.imageView?.image = UIImage(named: "play")
+//        playPauseButton.layer.zPosition = 1.0
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -38,12 +44,13 @@ class CarrersVC: UIViewController {
         avPlayerView.pause()
     }
     
-    @objc func mouseDidMoveOverView(_ sender: UILongPressGestureRecognizer){
+    @objc func mouseDidTapView(_ sender: UITapGestureRecognizer){
 
         if avPlayerView.isPlaying{
             avPlayerView.pause()
         }
         else{
+//            playPauseButton.isHidden = true
             avPlayerView.play()
         }
     }
@@ -57,6 +64,29 @@ extension AVPlayer {
         return false
     }
 }
+
+extension CarrersVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CarrersTableVIewCell.self)) as? CarrersTableVIewCell {
+            cell.domain.text = "New"
+            cell.role.text = "ascawc"
+            cell.location.text = "vasvasfbva"
+            print(cell.domain.text)
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+
 
 //extension UIImageView{
 //    
