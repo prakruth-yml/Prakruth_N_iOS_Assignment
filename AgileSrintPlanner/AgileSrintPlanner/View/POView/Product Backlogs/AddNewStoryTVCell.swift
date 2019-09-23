c028eee1f85087a5aed586f25510556767bbb708
prakruth-yml <prakruth.nagaraj@ymedialab.com>
2019-09-23 20:36:26 +0530

    Code refactoring review 1

---
+++ AgileSrintPlanner/AgileSrintPlanner/View/POView/Product Backlogs/AddNewStoryTVCell.swift
@@ -0,0 +1,25 @@
+
+//
+//  AddNewStoryTVCell.swift
+//  AgileSrintPlanner
+//
+//  Created by Prakruth Nagaraj on 23/09/19.
+//  Copyright © 2019 Prakruth Nagaraj. All rights reserved.
+//
+
+import UIKit
+
+class AddNewStoryTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
